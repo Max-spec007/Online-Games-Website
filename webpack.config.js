2597732c476ebf2f57430419b722418e5d21e642
Maxwell Kwarteng <maxtheflash007@gmail.com>
2021-05-20 00:20:44 -0400

    Added all the modified files to the Online-Games-Website Project Created all the config and lint files and modified the package.json.

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].bundle.js',
+    assetModuleFilename: 'assets/[name][hash][ext]'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_module/,
+        use: [
+          'babel-loader',
+          'stylelint-custom-processor-loader',
+        ],
+      },
+      {
+        test: /\.svg/,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  resolve: {
+    modules: ['node_modules', path.resolve(__dirname, 'src')],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  devtool: 'inline-cheap-source-map',
+  context: __dirname,
+  target: 'web',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+    compress: true,
+    hot: true
+  }
+};
+
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+//
+// module.exports = {
+//   mode: 'development',
+//   entry: './src/index.tsx',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: '[name].[contenthash].bundle.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.tsx?$/,
+//         exclude: /node_module/,
+//         use: [
+//           'babel-loader',
+//           'stylelint-custom-processor-loader',
+//         ]
+//       }
+//     ]
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin {
+//       template: './src/index.html'
+//     })
+//   ],
+//   resolve: {
+//     modules: ['node_modules', path.resolve(__dirname, 'src')],
+//     extensions: ['.js', '.jsx', '.ts', '.tsx']
+//   },
+//   devtool: 'inline-cheap-source-map',
+//   context: __dirname,
+//   target: 'web',
+//   devServer: {
+//     contentBase: path.join(__dirname, 'dist'),
+//     port: 3000,
+//     compress: true,
+//     hot: true
+//   }
+// }
